cb34580ed47d0e21879495ebe088a4b8bea68619
manueljenni <manuelnoahjenni@gmail.com>
2022-07-10 18:13:38 +0200

    Cleanup, package, create JAR

--- src/main/java/com/manueljenni/poker/App.java
+++ src/main/java/com/manueljenni/poker/App.java
@@ -1,7 +1,78 @@
 package com.manueljenni.poker;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+
+        HandComparer handComparer = new HandComparer();
+
+        Card card1 = Card.builder()
+                .rank(Rank.RANK_2)
+                .suit(Suit.D)
+                .build();
+
+        Card card2 = Card.builder()
+                .rank(Rank.RANK_3)
+                .suit(Suit.C)
+                .build();
+
+        Card card3 = Card.builder()
+                .rank(Rank.RANK_7)
+                .suit(Suit.C)
+                .build();
+
+        Card card4 = Card.builder()
+                .rank(Rank.RANK_Q)
+                .suit(Suit.D)
+                .build();
+
+        Card card5 = Card.builder()
+                .rank(Rank.RANK_4)
+                .suit(Suit.S)
+                .build();
+
+        Hand hand1 = Hand.builder()
+                .cards(List.of(card1, card2, card3, card4, card5))
+                .build();
+
+        Card card6 = Card.builder()
+                .rank(Rank.RANK_2)
+                .suit(Suit.D)
+                .build();
+
+        Card card7 = Card.builder()
+                .rank(Rank.RANK_3)
+                .suit(Suit.D)
+                .build();
+
+        Card card8 = Card.builder()
+                .rank(Rank.RANK_4)
+                .suit(Suit.D)
+                .build();
+
+        Card card9 = Card.builder()
+                .rank(Rank.RANK_A)
+                .suit(Suit.D)
+                .build();
+
+        Card card10 = Card.builder()
+                .rank(Rank.RANK_5)
+                .suit(Suit.D)
+                .build();
+
+        Hand hand2 = Hand.builder()
+                .cards(List.of(card6, card7, card8, card9, card10))
+                .build();
+
+        var winningHand = handComparer.getWinningHand(hand1, hand2);
+        String winningHandText;
+
+        if (winningHand == hand1) {
+            winningHandText = "Hand 1";
+        } else winningHandText = "Hand 2";
+
+        System.out.printf("Winning hand: hand %s%n", winningHandText);
 
     }
 }
